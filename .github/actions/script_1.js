36c40fc75c5f7b9a730f88c2bf9bc7b32765f180
matttrix <1185398+matttrix@users.noreply.github.com>
2021-01-13 17:55:18 +0800

    Update script_1.js

--- .github/actions/script_1.js
+++ .github/actions/script_1.js
@@ -1,1 +1,7 @@
-console.log((process.env.GITHUB_TOKEN || '').length)
+const dayjs = require('dayjs')
+const utcPlugin = require('dayjs/plugin/utc')
+const timezonePlugin = require('dayjs/plugin/timezone')
+dayjs.extend(utcPlugin)
+dayjs.extend(timezonePlugin)
+dayjs.tz.setDefault('Asia/Seoul')
+console.log(dayjs.tz().format())
