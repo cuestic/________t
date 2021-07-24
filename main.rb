60537c1df9460bff6f8854707b05ac9114534f93
matttrix <1185398+matttrix@users.noreply.github.com>
2021-07-24 19:42:53 +0800

    Update main.rb

--- main.rb
+++ main.rb
@@ -15,3 +15,8 @@ response = HTTP.get('https://www.googleapis.com/youtube/v3/search', params: {
 })
 
 puts JSON.parse(response.body)['items'].size
+
+new_data = { date: Time.parse("20#{date}").next_day.strftime('%y%m%d') }
+puts new_data
+
+HTTP.headers({ 'X-Master-Key': ENV['JSONBIN_TOKEN'] }).put('https://lumo.vercel.app/b/160220', json: new_data)
